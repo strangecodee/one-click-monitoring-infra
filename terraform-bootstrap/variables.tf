6b3141b8ae0ba3194303bc382ae195659e0b4e7f
strangecodee <anuragmaurya883@gmail.com>
2026-07-01 18:24:38 +0000

    Fix default variables in bootstrap configuration to match main backend state config

--- terraform-bootstrap/variables.tf
+++ terraform-bootstrap/variables.tf
@@ -1,11 +1,11 @@
 variable "backend_bucket_name" {
   description = "S3 bucket name for Terraform backend state"
   type        = string
-  default     = "anurag-monitoring-tfstate-ap-south-1"
+  default     = "monitoring-stack-dev-state"
 }
 
 variable "backend_lock_table_name" {
   description = "DynamoDB table name for Terraform state locking"
   type        = string
-  default     = "terraform-lock-table"
+  default     = "monitoring-stack-dev-lock"
 }
